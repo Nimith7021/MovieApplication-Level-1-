867225b13293900835e521b3bf3f949441a411cf
Nimith Gangadhar Shetty <Nimith.Shetty@asl.mum.sst>
2024-07-19 22:47:35 +0530

    initial commit

---
+++ Models/Movie.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApplication.Models
+{
+    internal class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+
+        public int YearOfRelease { get; set; }
+
+        public Movie(int id , string name , string genre , int year)
+        {
+            Id = id;
+            Name = name;
+            Genre = genre;
+            YearOfRelease = year;
+        }
+
+        public static Movie CreateNewMovieEntry(int id , string name , string Genre , int year)
+        {
+            return new Movie(id,name,Genre,year);
+        }
+
+        public override string ToString()
+        {
+            Console.WriteLine($"=================Movie Id : {Id}========================");
+            return $"Movie Id : {Id}\n" +
+                $"Movie Name : {Name}\n" +
+                $"Movie Genre {Genre}\n" +
+                $"Movie's Year Of Release : {YearOfRelease}\n";
+        }
+
+
+    }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,136 @@
+﻿using System.Threading.Channels;
+using MovieApplication.Models;
+namespace MovieApplication
+{
+    internal class Program
+    {
+        static List<Movie> movies = new List<Movie>();
+        static void Main(string[] args)
+        {
+            DisplayMovieMenu();
+        }
+
+        static void DisplayMovieMenu()
+        {
+            while (true)
+            {
+                Console.WriteLine("\nWelcome to the Movie Application\n" +
+                    "What do you wish to do ?\n" +
+                    "1 . Add new Movie \n" +
+                    "2 . Display All Movies \n" +
+                    "3 . Find Movie By Id \n" +
+                    "4 . Remove Movie by Name \n" +
+                    "5 . Clear All Movies \n" +
+                    "6 . Exit");
+
+                Console.WriteLine("Enter your choice");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                DoTask(choice);
+            }
+        }
+
+        static void DoTask(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    AddNewMovie();
+                    break;
+
+                case 2:
+                    if (movies.Count == 0)
+                        Console.WriteLine("Nothing to Display as there are no movies in List !!!");
+                    else
+                        movies.ForEach(movie => Console.WriteLine(movie));
+                    break;
+
+                case 3:
+                    Movie findMovie = FindMovieById();
+                    if (findMovie != null)
+                        Console.WriteLine(findMovie);
+                    else
+                        Console.WriteLine("Movie does not exist in the List");
+                    break;
+
+                case 4:
+                    RemoveMovieByName();
+                    break;
+
+                case 5:
+                    if (movies.Count == 0)
+                        Console.WriteLine("Nothing to Clear as there are no movies in List !!!");
+                    else
+                    {
+                        movies.Clear();
+                        Console.WriteLine("Cleared all movies successfully");
+                    }
+                    break;
+
+                case 6:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter a valid choice");
+                    break ;
+            }
+        }
+
+        static Movie AddNewMovie()
+        {
+            if (movies.Count > 5)
+            {
+                Console.WriteLine("Capacity exceeded ! Cant add movies");
+                return null;
+            }
+            else
+            {
+                Console.WriteLine("Enter your Movie Id");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter your Movie Name");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter your Movie Genre ");
+                string genre = Console.ReadLine();
+                Console.WriteLine("Enter the year of release");
+                int year = Convert.ToInt32(Console.ReadLine());
+
+                Movie newMovie = Movie.CreateNewMovieEntry(id, name, genre, year);
+                movies.Add(newMovie);
+                Console.WriteLine("MOVIE ADDED SUCCESSFULLY !!!");
+                return newMovie;
+            }
+        }
+
+        static Movie FindMovieById()
+        {
+            Movie findMovie = null;
+            Console.WriteLine("Enter your Movie Id:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            findMovie = movies.Where(item => item.Id==id).FirstOrDefault();
+            return findMovie;
+        }
+
+        static Movie FindMovieByName()
+        {
+            Movie findMovie = null;
+            Console.WriteLine("Enter your Movie Name:");
+            string name = Console.ReadLine();
+            findMovie = movies.Where(item => item.Name == name).FirstOrDefault();
+            return findMovie;
+        }
+
+        static void RemoveMovieByName()
+        {
+            Movie findMovie = FindMovieByName();
+            if(findMovie==null)
+                Console.WriteLine("Movie does not exist");
+            else
+            {
+                movies.Remove(findMovie);
+                Console.WriteLine("Movie Removed Successfully");
+            }
+        }
+
+
+    }
+}
